d7185c9c3ef4da8e097fe010c469f367cdf4405c
Narayan Nalbhe <145420297+narayannalbhe1@users.noreply.github.com>
2023-12-27 16:27:35 +0530

    Initial commit

---
+++ lib/LoginScreen.dart
@@ -0,0 +1,182 @@
+
+import 'dart:math';
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_notifications/Common%20Components/RoundButton.dart';
+import 'package:firebase_notifications/SignUp.dart';
+import 'package:firebase_notifications/utils/utils.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+final _formKey = GlobalKey<FormState>();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+
+  final _auth = FirebaseAuth.instance ;
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    emailController.dispose();
+    passwordController.dispose();
+  }
+
+  void login(){
+    _auth.signInWithEmailAndPassword(
+        email: emailController.text,
+        password: passwordController.text.toString()).then((value) {
+         Utils().toastMessage(value.user!.email.toString());
+
+
+    }).onError((error, stackTrace) {
+      Utils().toastMessage(error.toString());
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return WillPopScope(
+      onWillPop:_onWillPop,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.deepPurple,
+          automaticallyImplyLeading: true,
+          title: Center(child: Text('Login')),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Form(
+                key: _formKey,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    TextFormField(
+                      controller: emailController,
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: const InputDecoration(
+                          hintText: 'Email',
+                          prefixIcon: Icon(Icons.alternate_email,color: Colors.grey,),
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(10)),
+                          borderSide: BorderSide(color: Colors.grey,),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(10)),
+                          borderSide: BorderSide(color: Colors.green),
+                        ),
+
+                      ),
+                      validator: (value) {
+                        if(value!.isEmpty){
+                          return 'Enter email';
+                        }
+                        return null;
+                      },
+                    ),
+                    SizedBox(height: 30,),
+                    TextFormField(
+                      controller: passwordController,
+                      keyboardType: TextInputType.text,
+                      decoration: const InputDecoration(
+                          hintText: 'Password',
+                          prefixIcon: Icon(Icons.lock_open,color: Colors.grey,),
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(10)),
+                          borderSide: BorderSide(color: Colors.grey,),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(10)),
+                          borderSide: BorderSide(color: Colors.green),
+                        ),
+                      ),
+                      obscureText: true,
+                      validator: (value) {
+                        if(value!.isEmpty){
+                          return 'Enter password';
+                        }
+                        return null;
+                      },
+                    ),
+                    const  SizedBox(height: 50,),
+                    RoundButton(
+                      title:'Login',
+                      onTap:(){
+                          if(_formKey.currentState!.validate()){
+                            login();
+                          }
+                      }
+                    ),
+                    SizedBox(height: 20,),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text('Don\'t have an account ?'),
+                        TextButton(
+                          onPressed: () {
+                            Navigator.push(context,
+                            MaterialPageRoute(builder: (context) {
+                              return SignUp();
+                            },));
+                          },
+                          child: Text(
+                            'Sign Up',
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
+                )
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+   Future<bool> _onWillPop() async {
+   return (await showDialog(
+    context: context,
+    builder: (context) => AlertDialog(
+      title: Text('Confirm Exit'),
+      content: Text('Do you want to exit the app?'),
+      actions: <Widget>[
+        TextButton(
+          onPressed: () {
+            Navigator.of(context).pop(true);
+          },
+          child: Text('Yes'),
+        ),
+        TextButton(
+          onPressed: () {
+            Navigator.of(context).pop(false);
+          },
+          child: Text('No'),
+        ),
+      ],
+    ),
+    )) ??
+      false;
+}
+
+}
+
---
+++ lib/SignUp.dart
@@ -0,0 +1,155 @@
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_notifications/Common%20Components/RoundButton.dart';
+import 'package:firebase_notifications/LoginScreen.dart';
+import 'package:firebase_notifications/utils/utils.dart';
+import 'package:flutter/material.dart';
+class SignUp extends StatefulWidget {
+  const SignUp({super.key});
+
+  @override
+  State<SignUp> createState() => _SignUpState();
+}
+
+class _SignUpState extends State<SignUp> {
+
+  bool loading = false;
+  final _formKey = GlobalKey<FormState>();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+  FirebaseAuth _auth = FirebaseAuth.instance;
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    emailController.dispose();
+    passwordController.dispose();
+  }
+
+  void signUp() {
+    setState(() {
+      loading = true ;
+    });
+    _auth.createUserWithEmailAndPassword(
+        email: emailController.text.toString(),
+        password: passwordController.text.toString()).then((value) {
+      setState(() {
+        loading = false ;
+      });
+    }).onError((error, stackTrace) {
+      Utils().toastMessage(error.toString());
+      setState(() {
+        loading = false ;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.deepPurple,
+        title: Text('Sign Up'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Form(
+                key: _formKey,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    TextFormField(
+                      controller: emailController,
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: const InputDecoration(
+                        hintText: 'Email',
+                        prefixIcon: Icon(Icons.alternate_email,color: Colors.grey,),
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(10)),
+                          borderSide: BorderSide(color: Colors.grey,),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(10)),
+                          borderSide: BorderSide(color: Colors.green),
+                        ),
+                      ),
+                      validator: (value) {
+                        if(value!.isEmpty){
+                          return 'Enter email';
+                        }
+                        return null;
+                      },
+                    ),
+                    SizedBox(height: 30,),
+                    TextFormField(
+                      controller: passwordController,
+                      keyboardType: TextInputType.text,
+                      decoration: const InputDecoration(
+                        hintText: 'Password',
+                        prefixIcon: Icon(Icons.lock_open,color: Colors.grey,),
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(10)),
+                          borderSide: BorderSide(color: Colors.grey,),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(10)),
+                          borderSide: BorderSide(color: Colors.green),
+                        ),
+                      ),
+                      obscureText: true,
+                      validator: (value) {
+                        if(value!.isEmpty){
+                          return 'Enter password';
+                        }
+                        return null;
+                      },
+                    ),
+                    const  SizedBox(height: 50,),
+                    RoundButton(
+                        title:'SignUp',
+                        loading: loading,
+                        onTap:(){
+                          if(_formKey.currentState!.validate()){
+                            signUp();
+                          }
+                        }
+                    ),
+                    SizedBox(height: 20,),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text('Already have an account'),
+                        TextButton(
+                          onPressed: () {
+                            Navigator.push(
+                                context, MaterialPageRoute(builder: (context) {
+                                 return LoginScreen();
+                                },));
+                          },
+                          child: Text(
+                            'Login',
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
+                )
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+}
